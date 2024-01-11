6e39bae23ff349ca7500598a0cb2b90caf30c81d
jjkrasnick <109563164+jjkrasnick@users.noreply.github.com>
2024-01-10 16:46:10 -0800

    s
    s
    
    Co-Authored-By: jjkrasnick7 <114534410+jjkrasnick7@users.noreply.github.com>

--- console.js
+++ console.js
@@ -1,7 +1,7 @@
 // hi
-console.log("hello world")
-console.log("hello world")
-console.log("hello world")
+console.log("hello world");
+console.log("hello world");
+console.log("hello world");
 console.log("Hello World");
 console.log("Hello World");
 console.log("Hello World");
